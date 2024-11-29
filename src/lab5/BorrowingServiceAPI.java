1c7dacebf1a92faa0de22297b0352a831617e93e
Munashe Sydney <munashesydney@github.com>
2024-11-28 22:25:59 -0800

    Part 6 done

--- src/lab5/BorrowingServiceAPI.java
+++ src/lab5/BorrowingServiceAPI.java
@@ -2,7 +2,7 @@ package lab5;
 
 public interface BorrowingServiceAPI {
 	// Two methods to manage Member's books
-	public boolean borrowBook(Member member, Book book); 
-	public boolean returnBook(Member member, Book book);
+	public BorrowingBookResult borrowBook(Member member, Book book); 
+	public BorrowingBookResult returnBook(Member member, Book book);
 
 }
